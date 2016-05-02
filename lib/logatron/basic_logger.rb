c48ff6e6b7d27f27bfdcc8f7bd52414085523218
Justin Grimes <justin.mgrimes@gmail.com>
2016-05-02 13:37:12 -0400

    didn't like this change rubocop made '

--- lib/logatron/basic_logger.rb
+++ lib/logatron/basic_logger.rb
@@ -42,7 +42,7 @@ module Logatron
       ml = Logatron::BasicScopedLogger.new(self)
       start = Time.now
       begin
-        res = yield(ml)
+        res = block.call(ml)
         write(format_log(severity: severity, msg: msg, status: status, duration: milliseconds_elapsed(Time.now, start), inputs: source, request: request), SEVERITY_MAP[severity])
         res
       rescue Exception => e
